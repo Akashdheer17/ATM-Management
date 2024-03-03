47fcb598cbf78dc36659b79f9ee937edfc06d896
unknown <akashdheer17@gmail.com>
2024-03-03 15:40:33 +0530

    removed some extra code

--- ATM.cpp
+++ ATM.cpp
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-#include <math.h>
 
-//Functions
-void login();
+// Function prototypes
 void mainMenu();
 void checkBalance(float balance);
 float moneyDeposit(float balance);
@@ -13,54 +10,39 @@ void menuExit();
 void errorMessage();
 bool checkPin();
 
-
-//Main Code
 int main() {
-    //Local Declarations
+    // Local Declarations
     int option;
     float balance = 15000.00;
     int choose;
-
     bool again = true;
 
-    // insert code here...
-
     while (again) {
-    mainMenu();
+        mainMenu();
 
-    printf("--------------------------------------------\n");
-    printf("Your Selection:\t");
-    scanf("%d", &option);
-
+        printf("--------------------------------------------\n");
+        printf("Your Selection:\t");
+        scanf("%d", &option);
 
         switch (option) {
             case 1:
-            	system("CLS");
-            	if(checkPin()){
-            		 checkBalance(balance);
-				}
+                if (checkPin()) {
+                    checkBalance(balance);
+                }
                 break;
             case 2:
-            	system("CLS");
-               
-                	if(checkPin()){
-            	 balance = moneyDeposit(balance);
-				}
+                if (checkPin()) {
+                    balance = moneyDeposit(balance);
+                }
                 break;
             case 3:
-            	system("CLS");
-              
-                	if(checkPin()){
-              balance = moneyWithdraw(balance);
-				}
-                
+                if (checkPin()) {
+                    balance = moneyWithdraw(balance);
+                }
                 break;
-
             case 4:
-            	system("CLS");
                 menuExit();
                 return 0;
-
             default:
                 errorMessage();
                 break;
@@ -72,105 +54,71 @@ int main() {
         printf("2. No\n");
         scanf("%d", &choose);
 
-        system("CLS");
-
-
-
         if (choose == 2) {
             again = false;
             menuExit();
-
         }
-
-}
-
+    }
 
     return 0;
-}//main code
+}
 
-
-
-//Functions
-
+// Function definitions
 void mainMenu() {
-
     printf("----------> Welcome to ATM Banking <----------\n\n");
-    printf("----->Please choose one of the options below <-----\n\n");
+    printf("-----> Please choose one of the options below <-----\n\n");
     printf("1. Check Balance\n");
     printf("2. Deposit\n");
     printf("3. Withdraw\n");
     printf("4. Exit\n\n");
-
-}//Main Menu
+}
 
 bool checkPin() {
-	int pin;
- printf("Enter Your 4 digit Pin\n");
-scanf("%d", &pin);
- if(pin==1234){
- 	return true;
- }
-  printf("Your Pin Is Incorrect!!\n");
-  return false;
- }
+    int pin;
+    printf("Enter Your 4 digit Pin\n");
+    scanf("%d", &pin);
+    if (pin == 1234) {
+        return true;
+    }
+    printf("Your Pin Is Incorrect!!\n");
+    return false;
+}
 
 void checkBalance(float balance) {
-    printf("\n\nYour Available Balance is:   $%.2f\n\n", balance);
-
-}//Check Balance
+    printf("\n\nYour Available Balance is: $%.2f\n\n", balance);
+}
 
 float moneyDeposit(float balance) {
     float deposit;
     printf("Your Balance is: $%.2f\n\n", balance);
-    printf("    Enter your amount to Deposit\n");
+    printf("Enter your amount to Deposit: ");
     scanf("%f", &deposit);
-
-
     balance += deposit;
-
-    printf("\n    Your New Balance is:   $%.2f\n\n", balance);
+    printf("\nYour New Balance is: $%.2f\n\n", balance);
     return balance;
-
-}//money deposit
+}
 
 float moneyWithdraw(float balance) {
     float withdraw;
-    bool back = true;
-
-    printf("You choose to Withdraw a money\n");
+    printf("You choose to Withdraw money\n");
     printf("Your Balance is: $%.2f\n\n", balance);
-
-    while (back) {
-    printf("Enter your amount to withdraw:\n");
+    printf("Enter your amount to withdraw: ");
     scanf("%f", &withdraw);
-
-
-    if (withdraw < balance) {
-        back = false;
+    if (withdraw > balance) {
+        printf("Insufficient balance\n");
+    } else {
         balance -= withdraw;
-        printf("\nYour withdrawing money is:  $%.2f\n", withdraw);
-        printf("    Your New Balance is:   $%.2f\n\n", balance);
-
-    }
-
-        else  {
-
-        printf("You don't have enough money\n");
-        printf("Please contact to your Bank Customer Services\n");
-        printf("    Your Balance is:   $%.2f\n\n", balance);
-
-    }
+        printf("\nYour withdrawing money is: $%.2f\n", withdraw);
+        printf("Your New Balance is: $%.2f\n\n", balance);
     }
     return balance;
-
-
-}//money withdraw
+}
 
 void menuExit() {
     printf("--------------Take your receipt!!!------------------\n");
     printf("-----Thank you for using ATM Banking Machine!!!-----\n");
-}//exit menu
+}
 
-void errorMessage() {;
-    printf("!!!You selected invalid number!!!\n");
-}//error message
+void errorMessage() {
+    printf("!!!You selected an invalid number!!!\n");
+}
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # ATM-Management
 The ATM System project implemented in Java allows users to perform various banking transactions such as withdrawing cash, depositing funds, checking balances, and transferring money between accounts.
+
+## Features
+
+- **Check Balance**: Users can check their account balance.
+- **Deposit**: Users can deposit money into their account.
+- **Withdraw**: Users can withdraw money from their account.
+- **PIN Verification**: Users need to enter a 4-digit PIN to access the system.
+- **Menu Navigation**: Users can navigate through the menu to perform different operations.
+- **Exit**: Users can exit the system.
+
+## Prerequisites
+
+To run this program, you need to have a C compiler installed on your system.
+
+## How to Use
+
+1. Clone this repository to your local machine.
+2. Compile the `ATM.cpp` file using a C compiler.
+3. Run the compiled executable file.
+4. Follow the instructions on the screen to navigate through the ATM system.
+5. Enter the PIN (default PIN is 1234).
+6. Perform transactions such as checking balance, depositing money, or withdrawing money.
+7. Exit the system when you are done.
+
+## Files Included
+
+- **ATM.cpp**: Contains the main code for the ATM system.
+- **README.md**: Provides information about the ATM system and how to use it.
+
+## Contribution
+
+Feel free to contribute to this project by submitting pull requests or reporting issues.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
